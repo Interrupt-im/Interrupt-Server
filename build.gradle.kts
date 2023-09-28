4cb33e8d79dbcaa91b38f5732b2cb7f0202c4896
junseo <djawnstj44@naver.com>
2023-09-29 01:39:59 +0900

    Feat: 프로젝트 생성
    스프링 부트 프로젝트를 생성했다.
    [이슈](https://github.com/Interrupt-im/Interrupt-Server/issues/3)에 적은 플러그인, 디펜던시, 버전으로 구성했다.
    초기 생성부터 local, test, prod 환경을 분리했고 일단 local 에서만 사용할 설정을 명시했다.
    또한 개발 편의성을 위해 일단은 시큐리티 디펜던시를 주석처리 해두었다.

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.9.0"
+
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
+group = "com.interrupt"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // DB
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // 테스트
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.mockk:mockk:1.13.8")
+
+    // 시큐리티
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+//    testImplementation("org.springframework.security:spring-security-test")
+
+    // 로깅
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/interrupt/server/InterruptServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.interrupt.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterruptServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
