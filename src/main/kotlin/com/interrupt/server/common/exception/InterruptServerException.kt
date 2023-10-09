775cc6c92d64fd5229e98f24794e994c55e710e6
junseo <djawnstj44@naver.com>
2023-10-09 23:42:51 +0900

    Feat: 예외 코드 생성, 예외 처리 로직 수정
    예외가 발생하면 로그를 남겨 예외 정보를 확인할 수 있도록 추가했다.
    이메일 인증, 이메일 인증코드, 회원 찾기 와 관련된 예외코드를 추가했다.

--- src/main/kotlin/com/interrupt/server/common/exception/InterruptServerException.kt
+++ src/main/kotlin/com/interrupt/server/common/exception/InterruptServerException.kt
@@ -1,10 +1,26 @@
 package com.interrupt.server.common.exception
 
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging
+
 class InterruptServerException @JvmOverloads constructor(
     message: String? = null,
     override val cause: Throwable? = null,
     val errorCode: ErrorCode,
 ): RuntimeException(message, cause) {
+
+    private val log: KLogger = KotlinLogging.logger {  }
+
+    init {
+        log.error {
+            """
+                server error
+                cause: $cause
+                message: $message
+                errorCode: $errorCode
+            """.trimIndent()
+        }
+    }
 
     override val message: String? = message
         get() = if (field.isNullOrBlank()) errorCode.message else "${errorCode.message}, $field"
