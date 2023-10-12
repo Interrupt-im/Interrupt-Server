5e7f43444b1e9576fdb1b37df5b298f996abd37a
JUNSEO <djawnstj44@naver.com>
2023-10-12 16:08:50 +0900

    Test: MemberService 통합테스트 메서드 추가
    회원가입, 아이디 중복체크, 이메일 인증코드 발송 에 대한 통합 테스트 코드를 작성하였다.

--- src/test/kotlin/com/interrupt/server/member/service/MemberServiceIntegrationTest.kt
+++ src/test/kotlin/com/interrupt/server/member/service/MemberServiceIntegrationTest.kt
@@ -1,15 +1,24 @@
 package com.interrupt.server.member.service
 
 import com.interrupt.server.IntegrationTestSupport
-import com.interrupt.server.common.exception.ErrorCode
-import com.interrupt.server.common.exception.InterruptServerException
-import com.interrupt.server.member.dto.delete.MemberDeleteRequest
-import com.interrupt.server.member.dto.login.MemberLoginRequest
+import com.interrupt.server.common.security.StringEncoder
+import com.interrupt.server.email.dto.EmailContent
+import com.interrupt.server.email.dto.EmailType
+import com.interrupt.server.email.entity.EmailMessage
+import com.interrupt.server.email.service.EmailSendService
+import com.interrupt.server.member.dto.duplicatedidcheck.LoginIdDuplicateCheckRequest
+import com.interrupt.server.member.dto.emailverify.EmailVerificationApplyRequest
 import com.interrupt.server.member.dto.register.MemberRegisterRequest
+import com.interrupt.server.member.entity.EmailVerifyCode
+import com.interrupt.server.member.repository.EmailVerifyCodeRepository
+import com.interrupt.server.member.repository.MemberRecoverRepository
+import com.interrupt.server.member.repository.MemberRepository
+import com.ninjasquad.springmockk.SpykBean
+import io.mockk.justRun
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
@@ -18,99 +27,59 @@ class MemberServiceIntegrationTest: IntegrationTestSupport() {
     @Autowired
     private lateinit var memberService: MemberService
 
+    @Autowired
+    private lateinit var memberRepository: MemberRepository
+    @SpykBean
+    private lateinit var emailSendService: EmailSendService
+    @Autowired
+    private lateinit var emailVerifyCodeRepository: EmailVerifyCodeRepository
+    @Autowired
+    private lateinit var memberRecoverRepository: MemberRecoverRepository
+    @Autowired
+    private lateinit var stringEncoder: StringEncoder
+
+    @Value("\${security.encrypt-key}")
+    private lateinit var secretKey: String
+
     @Test
     fun `회원 가입 dto 를 통해 받은 정보로 회원 가입을 수행 한다`() {
         // given
-        val testMemberDto = MemberRegisterRequest("test1", "testPassword", "testName", "test@mail.com")
+        val email = "test@mail.com"
+        val emailVerifyCodeKey = "0000"
+        val encryptedEmail = stringEncoder.encrypt(email, secretKey)
 
-        // when
-        val savedMember = memberService.registerMember(testMemberDto)
-
-        // then
-        assertThat(savedMember)
-            .isNotNull
-            .extracting("loginId", "name", "email")
-            .contains(testMemberDto.loginId, testMemberDto.name, testMemberDto.email)
-    }
-
-    @Test
-    fun `이미 존재하는 ID 로 회원가입을 시도하면 그에 맞는 에러를 발생한다`() {
-        // given
-        val testMemberDto1 = MemberRegisterRequest("test1", "testPassword", "testName1", "test1@mail.com")
-        val testMemberDto2 = MemberRegisterRequest("test1", "testPassword", "testName2", "test2@mail.com")
-
-        memberService.registerMember(testMemberDto1)
-
-        // when
-        val result = assertThatThrownBy { memberService.registerMember(testMemberDto2) }
-
-        // then
-        result
-            .isInstanceOf(InterruptServerException::class.java)
-            .hasMessage(ErrorCode.DUPLICATED_REGISTER_LOGIN_ID.message)
-    }
-
-    @Test
-    fun `회원 ID 와 비밀번호를 전달받아 로그인 로직을 수행 후 회원 이름을 반환한다`() {
-        // given
-        val loginId = "test1"
-        val password = "testPassword"
-        val name = "testName"
-        val memberRegisterRequest = MemberRegisterRequest(loginId, password, name, "test@mail.com")
-        val memberLoginRequest = MemberLoginRequest(loginId, password)
-
-        memberService.registerMember(memberRegisterRequest)
-
-        // when
-        val memberLoginResponse = memberService.login(memberLoginRequest)
-
-        // then
-        assertThat(memberLoginResponse.name).isEqualTo(name)
-    }
-
-    @Test
-    fun `존재하지 않는 회원 ID 와 비밀번호로 로그인을 시도하면 적절한 예외를 반환`() {
-        // given
-        val loginId = "test1"
-        val password = "testPassword"
-        val memberLoginRequest = MemberLoginRequest(loginId, password)
-
-        // when
-        val result = assertThatThrownBy { memberService.login(memberLoginRequest) }
-
-        // then
-        result.isInstanceOf(InterruptServerException::class.java)
-            .hasMessage(ErrorCode.FAILED_LOGIN.message)
-    }
-
-    @Test
-    fun `회원 정보를 받아 해당되는 회원 탈퇴 비지니스 로직을 수행한다`() {
-        // given
-        val loginId = "test1"
-        val password = "testPassword"
-        val memberRegisterRequest = MemberRegisterRequest(loginId, password, "testName", "test@mail.com")
-        val memberDeleteRequest = MemberDeleteRequest(loginId, password)
-
-        memberService.registerMember(memberRegisterRequest)
+        val verifyCode = emailVerifyCodeRepository.save(EmailVerifyCode(encryptedEmail, "000000", true))
+        val testMemberDto = MemberRegisterRequest("test1", "testPassword", "testName", email, verifyCode.uuid)
 
         // when then
-        memberService.deleteMember(memberDeleteRequest)
-
+        memberService.registerMember(testMemberDto)
     }
 
     @Test
-    fun `존재하지 않는 회원 ID 와 비밀번호로 회원탈퇴를 시도하면 적절한 예외를 반환`() {
+    fun `아이디 중복체크 요청을 받아 중복체크를 수행한다`() {
         // given
-        val loginId = "test1"
-        val password = "testPassword"
-        val memberDeleteRequest = MemberDeleteRequest(loginId, password)
+        val request = LoginIdDuplicateCheckRequest("test")
 
         // when
-        val result = assertThatThrownBy { memberService.deleteMember(memberDeleteRequest) }
+        val response = memberService.checkLoginIdDuplication(request)
 
         // then
-        result.isInstanceOf(InterruptServerException::class.java)
-            .hasMessage(ErrorCode.FAILED_LOGIN.message)
+        assertThat(response.isUnique).isTrue()
+    }
+
+    @Test
+    fun `이메일 인증 코드 발송 요청을 받아 인증코드를 발송한다`() {
+        // given
+        val email = "djawnstj44@naver.com"
+        val request = EmailVerificationApplyRequest(email)
+
+        justRun { emailSendService.sendMail(EmailMessage(email, EmailContent(EmailType.MEMBER_REGISTER.subject, "testContent"))) }
+
+        // when
+        val response = memberService.applySendEmailVerifyCode(request)
+
+        // then
+        assertThat(response.emailVerifyCodeKey).isNotNull()
     }
 
 }
