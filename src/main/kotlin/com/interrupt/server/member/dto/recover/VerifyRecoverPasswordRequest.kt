61084711a31454d7fec54a58f8689ec3fb405542
junseo <djawnstj44@naver.com>
2023-10-12 23:43:24 +0900

    Refactor: 비밀번호 찾기 요청 Dto 필드명 수정

--- src/main/kotlin/com/interrupt/server/member/dto/recover/VerifyRecoverPasswordRequest.kt
+++ src/main/kotlin/com/interrupt/server/member/dto/recover/VerifyRecoverPasswordRequest.kt
@@ -3,7 +3,7 @@ package com.interrupt.server.member.dto.recover
 data class VerifyRecoverPasswordRequest(
     val memberRecoverKey: String,
     val verifyCode: String,
-    val password: String,
+    val newPassword: String,
 ) {
 
 }
