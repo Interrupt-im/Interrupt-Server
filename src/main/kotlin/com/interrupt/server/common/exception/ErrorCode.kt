8ee007a7e367d5c7231828ee8d8f363d2e578c48
JUNSEO <djawnstj44@naver.com>
2023-10-13 16:50:35 +0900

    Feat: 검증 실패에 대한 예외 핸들러 추가

--- src/main/kotlin/com/interrupt/server/common/exception/ErrorCode.kt
+++ src/main/kotlin/com/interrupt/server/common/exception/ErrorCode.kt
@@ -16,6 +16,9 @@ enum class ErrorCode(
     EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "인증된 이메일이 아닙니다.", "ME0005"),
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "일치하는 회원 정보를 찾을 수 없습니다.", "ME0006"),
 
+    // Validation
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.", "VE0001"),
+
     // COMMON
     INTERNAL_SEVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "관리자에게 문의 바랍니다.", "IE0002"),
     ;
--- src/main/kotlin/com/interrupt/server/common/exception/GlobalRestControllerAdvice.kt
+++ src/main/kotlin/com/interrupt/server/common/exception/GlobalRestControllerAdvice.kt
@@ -1,7 +1,9 @@
 package com.interrupt.server.common.exception
 
-import com.interrupt.server.common.api.BaseResponse
+import com.interrupt.server.common.api.ExceptionResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -9,11 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalRestControllerAdvice {
 
     @ExceptionHandler(InterruptServerException::class)
-    fun globalServerExceptionHandler(e: InterruptServerException): ResponseEntity<BaseResponse<*>> =
-        ResponseEntity(BaseResponse(e.errorCode.status.value(), e.errorCode.status, e.message), e.errorCode.status)
+    fun globalServerExceptionHandler(e: InterruptServerException): ResponseEntity<ExceptionResponse<*>> =
+        ResponseEntity(ExceptionResponse(e.errorCode.status.value(), e.errorCode, e.message, null), e.errorCode.status)
 
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ExceptionResponse<*>> =
+        InterruptServerException(ErrorCode.INVALID_INPUT_VALUE.message, e, ErrorCode.INVALID_INPUT_VALUE)
+            .let { ex ->
+                ResponseEntity(
+                    ExceptionResponse(
+                        ex.errorCode.status.value(),
+                        ex.errorCode,
+                        ex.message,
+                        e.bindingResult.allErrors
+                            .filterIsInstance<FieldError>()
+                            .associate { it.field to it.defaultMessage }
+                    ),
+                    ex.errorCode.status)
+            }
     @ExceptionHandler(Throwable::class)
-    fun throwableHandler(t: Throwable): ResponseEntity<BaseResponse<*>> =
-        InterruptServerException(cause = t, errorCode = ErrorCode.INTERNAL_SEVER_ERROR).let { ResponseEntity(BaseResponse(it.errorCode.status.value(), it.errorCode.status, it.message), it.errorCode.status) }
+    fun throwableHandler(t: Throwable): ResponseEntity<ExceptionResponse<*>> =
+        InterruptServerException(cause = t, errorCode = ErrorCode.INTERNAL_SEVER_ERROR)
+            .let { ResponseEntity(ExceptionResponse(it.errorCode.status.value(), it.errorCode, it.message, null), it.errorCode.status) }
 
 }
