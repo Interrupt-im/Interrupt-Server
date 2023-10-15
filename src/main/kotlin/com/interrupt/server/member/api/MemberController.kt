dd77043c96481cecdc55be4f9154bf96c94813ef
junseo <djawnstj44@naver.com>
2023-10-15 23:34:54 +0900

    Feat: 회원 도메인 api 개발

--- src/main/kotlin/com/interrupt/server/member/api/MemberController.kt
+++ src/main/kotlin/com/interrupt/server/member/api/MemberController.kt
@@ -1,12 +1,15 @@
 package com.interrupt.server.member.api
 
 import com.interrupt.server.common.api.BaseResponse
+import com.interrupt.server.member.dto.delete.MemberDeleteRequest
 import com.interrupt.server.member.dto.duplicatedidcheck.LoginIdDuplicateCheckRequest
 import com.interrupt.server.member.dto.duplicatedidcheck.LoginIdDuplicateCheckResponse
+import com.interrupt.server.member.dto.register.MemberRegisterRequest
+import com.interrupt.server.member.dto.update.MemberUpdateRequest
 import com.interrupt.server.member.service.MemberService
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class MemberController(
@@ -14,7 +17,22 @@ class MemberController(
 ) {
 
     @GetMapping("/api/v1/members/check-login-id")
-    fun checkLoginId(@Valid request: LoginIdDuplicateCheckRequest): BaseResponse<LoginIdDuplicateCheckResponse> =
-        BaseResponse(message = "OK", data = memberService.checkLoginIdDuplication(request))
+    fun checkLoginIdDuplication(@Valid request: LoginIdDuplicateCheckRequest): BaseResponse<LoginIdDuplicateCheckResponse> =
+        BaseResponse(data = memberService.checkLoginIdDuplication(request))
+
+    @PostMapping("/api/v1/members")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerMember(@RequestBody @Valid request: MemberRegisterRequest): BaseResponse<*> =
+        BaseResponse(statusCode = HttpStatus.CREATED.value(), data = memberService.registerMember(request))
+
+    @PatchMapping("/api/v1/members/{loginId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateMember(@PathVariable("loginId") loginId: String, @RequestBody @Valid request: MemberUpdateRequest): BaseResponse<*> =
+        BaseResponse(statusCode = HttpStatus.NO_CONTENT.value(), data = memberService.updateMember(request.apply { this.loginId = loginId }))
+
+    @DeleteMapping("/api/v1/members/{loginId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun foo(@PathVariable("loginId") loginId: String, @RequestBody @Valid request: MemberDeleteRequest): BaseResponse<*> =
+        BaseResponse(statusCode = HttpStatus.NO_CONTENT.value(), data = memberService.deleteMember(request.apply { this.loginId = loginId }))
 
 }
