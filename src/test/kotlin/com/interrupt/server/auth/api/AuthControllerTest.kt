8abe857e62c95b7d0c8e25e671755c45dee5414d
JUNSEO <djawnstj44@naver.com>
2023-10-23 11:21:30 +0900

    Test: AuthController 테스트 코드 완료

--- src/test/kotlin/com/interrupt/server/auth/api/AuthControllerTest.kt
+++ src/test/kotlin/com/interrupt/server/auth/api/AuthControllerTest.kt
@@ -6,12 +6,12 @@ import com.interrupt.server.member.dto.emailverify.EmailVerificationApplyRespons
 import com.interrupt.server.member.dto.emailverify.EmailVerifyRequest
 import com.interrupt.server.member.dto.login.MemberLoginRequest
 import com.interrupt.server.member.dto.login.MemberLoginResponse
-import com.interrupt.server.member.dto.recover.RecoverLoginIdRequest
+import com.interrupt.server.member.dto.recover.*
 import io.mockk.every
 import io.mockk.justRun
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -49,7 +49,7 @@ class AuthControllerTest: ControllerTestSupport() {
     }
 
     @Test
-    fun `이메일 인증 코드 발송 요청 시 이메일 주소는 올바른 형식 ㄷ이어야 한다`() {
+    fun `이메일 인증 코드 발송 요청 시 이메일 주소는 올바른 형식 이어야 한다`() {
         // given
         val request = EmailVerificationApplyRequest("email")
 
@@ -211,7 +211,7 @@ class AuthControllerTest: ControllerTestSupport() {
     }
 
     @Test
-    fun `로그인 시 비밀번호 는 영문 또는 숫자가 모두 포함되어야 한다`() {
+    fun `로그인 시 비밀번호 는 영문과 숫자가 모두 포함되어야 한다`() {
         // given
         val request = MemberLoginRequest("memberId", "password")
 
@@ -219,6 +219,257 @@ class AuthControllerTest: ControllerTestSupport() {
         mockMvc.perform(
             post("/api/v1/auth/login")
                 .content(objectMapper.writeValueAsBytes(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isInvalidInputValueResponse("password")
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 발송 요청`() {
+        // given
+        val request = RecoverLoginIdRequest("홍길동", "email@domain.com")
+
+        every { memberService.applySendLoginIdRecoverVerifyCode(request) } returns RecoverLoginIdResponse("0000")
+
+        // when then
+        mockMvc.perform(
+            post("/api/v1/auth/login-id/recovery/verify-code")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isOkBaseResponse()
+            .andExpect(jsonPath("$.data").isNotEmpty)
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 발송 요청 시 이름 값은 필수이다`() {
+            // given
+            val request = RecoverLoginIdRequest("", "email@domain.com")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/login-id/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("name")
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 발송 요청 시 이메일 값은 필수이다`() {
+            // given
+            val request = RecoverLoginIdRequest("홍길동", "")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/login-id/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("email")
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 발송 요청 시 이메일 값은 올바른 형식 이어야 한다`() {
+            // given
+            val request = RecoverLoginIdRequest("홍길동", "email")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/login-id/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("email")
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 확인`() {
+        // given
+        val request = VerifyRecoverLoginIdRequest("0000", "000000")
+        val loginId = "memberId"
+
+        every { memberService.validateLoginIdRecoverVerifyCode(request) } returns VerifyRecoverLoginIdResponse(loginId)
+
+        // when then
+        mockMvc.perform(
+            get("/api/v1/auth/login-id")
+                .param("memberRecoverKey", request.memberRecoverKey)
+                .param("verifyCode", request.verifyCode))
+            .andDo(print())
+            .isOkBaseResponse()
+            .andExpect(jsonPath("$.data").isNotEmpty)
+            .andExpect(jsonPath("$.data.loginId").value(loginId))
+    }
+
+    @Test
+    fun `회원 ID 찾기 이메일 인증 코드 확인 시 이메일 인증 코드 값은 필수이다`() {
+        // given
+        val request = VerifyRecoverLoginIdRequest("0000", "")
+
+        // when then
+        mockMvc.perform(
+            get("/api/v1/auth/login-id")
+                .param("memberRecoverKey", request.memberRecoverKey)
+                .param("verifyCode", request.verifyCode))
+            .andDo(print())
+            .isInvalidInputValueResponse("verifyCode")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청`() {
+        // given
+        val request = RecoverPasswordRequest("memberId", "email@domain.com")
+
+        every { memberService.applySendPasswordRecoverVerifyCode(request) } returns RecoverPasswordResponse("0000")
+
+        // when then
+        mockMvc.perform(
+            post("/api/v1/auth/password/recovery/verify-code")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isOkBaseResponse()
+            .andExpect(jsonPath("$.data").isNotEmpty)
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청 시 회원 ID 값은 필수이다`() {
+            // given
+        val request = RecoverPasswordRequest("", "email@domain.com")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/password/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("loginId")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청 시 회원 ID 값은 8~20자로 이루어져아 한다`() {
+            // given
+        val request = RecoverPasswordRequest("loginId", "email@domain.com")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/password/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("loginId")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청 시 회원 ID 값은 영문 또는 숫자로 이루어져야 한다`() {
+            // given
+        val request = RecoverPasswordRequest("회원ID", "email@domain.com")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/password/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("loginId")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청 시 이메일 값은 필수이다`() {
+            // given
+        val request = RecoverPasswordRequest("memberId", "")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/password/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("email")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 발송 요청 시 이메일 값은 올바른 형식 이어야 한다`() {
+            // given
+        val request = RecoverPasswordRequest("memberId", "email")
+
+            // when then
+            mockMvc.perform(
+                post("/api/v1/auth/password/recovery/verify-code")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .isInvalidInputValueResponse("email")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 확인`() {
+        // given
+        val request = VerifyRecoverPasswordRequest("0000", "000000", "newP123!")
+
+        justRun { memberService.validatePasswordRecoverVerifyCode(request) }
+
+        // when then
+        mockMvc.perform(
+            put("/api/v1/auth/member/{loginId}/password", "memberId")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isNoContent)
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 확인 시 이메일 인증 코드 값은 필수이다`() {
+        // given
+        val request = VerifyRecoverPasswordRequest("0000", "", "word123!")
+
+        // when then
+        mockMvc.perform(
+            put("/api/v1/auth/member/{loginId}/password", "memberId")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isInvalidInputValueResponse("verifyCode")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 확인 시 비밀번호 값은 필수이다`() {
+        // given
+        val request = VerifyRecoverPasswordRequest("0000", "000000", "")
+
+        // when then
+        mockMvc.perform(
+            put("/api/v1/auth/member/{loginId}/password", "memberId")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isInvalidInputValueResponse("password")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 확인 시 비밀번호 는 8~20자로 이루어져아 한다`() {
+        // given
+        val request = VerifyRecoverPasswordRequest("0000", "000000", "word123")
+
+        // when then
+        mockMvc.perform(
+            put("/api/v1/auth/member/{loginId}/password", "memberId")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .isInvalidInputValueResponse("password")
+    }
+
+    @Test
+    fun `비밀번호 찾기 이메일 인증 코드 확인 시 비밀번호 는 영문과 숫자가 모두 포함되어야 한다`() {
+        // given
+        val request = VerifyRecoverPasswordRequest("0000", "000000", "password")
+
+        // when then
+        mockMvc.perform(
+            put("/api/v1/auth/member/{loginId}/password", "memberId")
+                .content(objectMapper.writeValueAsString(request))
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .isInvalidInputValueResponse("password")
