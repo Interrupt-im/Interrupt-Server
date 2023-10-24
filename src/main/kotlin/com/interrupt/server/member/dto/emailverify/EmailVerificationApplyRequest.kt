04f4972ab7574e9a20be7d3c8f2784a1c180da27
JUNSEO <djawnstj44@naver.com>
2023-10-24 16:58:34 +0900

    Feat: validation 애너테이션 생성

--- src/main/kotlin/com/interrupt/server/member/dto/emailverify/EmailVerificationApplyRequest.kt
+++ src/main/kotlin/com/interrupt/server/member/dto/emailverify/EmailVerificationApplyRequest.kt
@@ -1,11 +1,13 @@
 package com.interrupt.server.member.dto.emailverify
 
+import com.interrupt.server.member.validation.annotation.email.EmailValidation
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
 data class EmailVerificationApplyRequest(
-    @field:NotBlank(message = "이메일을 입력하지 않았습니다.")
-    @field:Email(message = "올바른 이메일 형식이 아닙니다.")
+    @EmailValidation
+    @field:NotBlank(message = "이메일 값은 필수 입니다.")
+    @field:Email(message = "올바른 이메일 형식으로 입력하셔야 합니다.")
     val email: String?
 ) {
 
