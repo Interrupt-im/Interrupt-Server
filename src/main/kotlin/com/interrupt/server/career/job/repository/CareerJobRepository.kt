f0d36437a0c6381029869f5fff20c82bc81ae544
JUNSEO <djawnstj44@naver.com>
2023-10-26 17:27:43 +0900

    Feat: 직무 Repository 를 Kotlin JDSL Repository 로 사용하기 위한 상속 추가

--- src/main/kotlin/com/interrupt/server/career/job/repository/CareerJobRepository.kt
+++ src/main/kotlin/com/interrupt/server/career/job/repository/CareerJobRepository.kt
@@ -1,9 +1,10 @@
 package com.interrupt.server.career.job.repository
 
 import com.interrupt.server.career.job.entity.Job
+import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CareerJobRepository: JpaRepository<Job, Long> {
+interface CareerJobRepository: JpaRepository<Job, Long>, KotlinJdslJpqlExecutor {
 }
