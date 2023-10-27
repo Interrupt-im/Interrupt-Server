967317976b6554c6d5c1ff62818e429e95622787
JUNSEO <djawnstj44@naver.com>
2023-10-27 17:28:37 +0900

    Feat: 직무, 직군 DTO 레디스 저장을 위한 애너테이션 추가

--- src/main/kotlin/com/interrupt/server/career/job/dto/JobDto.kt
+++ src/main/kotlin/com/interrupt/server/career/job/dto/JobDto.kt
@@ -1,5 +1,8 @@
 package com.interrupt.server.career.job.dto
 
+import com.interrupt.server.common.annotation.RedisEntity
+
+@RedisEntity
 data class JobDto(
     val id: Long,
     val name: String,
