b4a13bdef487e7f881e98c7590ceaebe2687f8f5
junseo <djawnstj44@naver.com>
2023-10-30 08:01:44 +0900

    Feat: 경력 DTO 생성

---
+++ src/main/kotlin/com/interrupt/server/career/dto/CareerDto.kt
@@ -0,0 +1,28 @@
+package com.interrupt.server.career.dto
+
+import com.interrupt.server.career.entity.Career
+import com.interrupt.server.career.skill.dto.SkillDto
+import com.interrupt.server.member.dto.MemberDto
+import java.time.LocalDate
+
+data class CareerDto(
+    val id: Long? = null,
+    val member: MemberDto,
+    val jobList: MutableList<CareerJobDto> = mutableListOf(),
+    val skillList: MutableList<SkillDto> = mutableListOf(),
+    val careerStartDate: LocalDate,
+    val careerEndDate: LocalDate,
+    val isPublic: Boolean = true,
+) {
+
+    fun toEntity(): Career = Career(
+        member = this.member.toEntity(),
+        careerStartDate = this.careerStartDate,
+        careerEndDate = this.careerEndDate,
+        isPublic = this.isPublic
+    )
+
+    companion object {
+    }
+
+}
---
+++ src/main/kotlin/com/interrupt/server/career/dto/CareerJobDto.kt
@@ -0,0 +1,13 @@
+package com.interrupt.server.career.dto
+
+import com.interrupt.server.career.entity.CareerJob
+import com.interrupt.server.career.job.dto.JobDto
+
+data class CareerJobDto(
+    val career: CareerDto,
+    val job: JobDto
+) {
+
+    fun toEntity(): CareerJob = CareerJob(career.toEntity(), job.toEntity())
+
+}
