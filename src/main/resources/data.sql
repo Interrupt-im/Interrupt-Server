200419ccc1c7d4a851996a00b5f4c50e8f3c87bf
JUNSEO <djawnstj44@naver.com>
2023-10-30 17:29:06 +0900

    Docs: 기술, 직무에 대한 기본 데이터 주입을 위해 쿼리 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- job
 insert into job_group(name, created_at, modified_at)
 values ('개발', now(), now());
 
@@ -15,3 +16,41 @@ insert into job(name, job_group_id, created_at, modified_at)
 values ('게임 클라이언트 개발자', 2, now(), now());
 insert into job(name, job_group_id, created_at, modified_at)
 values ('모바일 게임 개발자', 2, now(), now());
+
+
+-- skill
+insert into skill_group(name, created_at, modified_at)
+values ('언어', now(), now());
+
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Java', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Kotlin', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('JavaScript', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('TypeScript', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Python', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('C', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('C++', 1, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('C#', 1, now(), now());
+
+insert into skill_group(name, created_at, modified_at)
+values ('프레임워크', now(), now());
+
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Spring', 2, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Nest', 2, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Next', 2, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('React', 2, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Vue', 2, now(), now());
+insert into skill(name, skill_group_id, created_at, modified_at)
+values ('Angular', 2, now(), now());
