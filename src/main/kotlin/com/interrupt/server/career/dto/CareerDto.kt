d61dd568507a43b029bd9c72253969a9d1fae268
JUNSEO <djawnstj44@naver.com>
2023-10-30 17:30:36 +0900

    Feat: 경력 별 제목 컬럼 추가

--- src/main/kotlin/com/interrupt/server/career/dto/CareerDto.kt
+++ src/main/kotlin/com/interrupt/server/career/dto/CareerDto.kt
@@ -1,15 +1,13 @@
 package com.interrupt.server.career.dto
 
 import com.interrupt.server.career.entity.Career
-import com.interrupt.server.skill.dto.SkillDto
 import com.interrupt.server.member.dto.MemberDto
 import java.time.LocalDate
 
 data class CareerDto(
     val id: Long? = null,
     val member: MemberDto,
-    val jobList: MutableList<CareerJobDto> = mutableListOf(),
-    val skillList: MutableList<SkillDto> = mutableListOf(),
+    val title: String,
     val careerStartDate: LocalDate,
     val careerEndDate: LocalDate,
     val isPublic: Boolean = true,
@@ -17,6 +15,7 @@ data class CareerDto(
 
     fun toEntity(): Career = Career(
         member = this.member.toEntity(),
+        title = this.title,
         careerStartDate = this.careerStartDate,
         careerEndDate = this.careerEndDate,
         isPublic = this.isPublic
--- src/main/kotlin/com/interrupt/server/career/entity/Career.kt
+++ src/main/kotlin/com/interrupt/server/career/entity/Career.kt
@@ -2,12 +2,7 @@ package com.interrupt.server.career.entity
 
 import com.interrupt.server.common.entity.SoftDeleteBaseEntity
 import com.interrupt.server.member.entity.Member
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToMany
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import java.time.LocalDate
 
 @Entity
@@ -15,9 +10,11 @@ import java.time.LocalDate
     name = "career"
 )
 class Career(
-    @field:OneToOne
+    @field:ManyToOne
     @field:JoinColumn(name = "member_id")
     val member: Member? = null,
+    @field:Column(name = "title")
+    val title: String,
     @field:Column(name = "career_start_date")
     val careerStartDate: LocalDate,
     @field:Column(name = "career_end_date", nullable = true)
