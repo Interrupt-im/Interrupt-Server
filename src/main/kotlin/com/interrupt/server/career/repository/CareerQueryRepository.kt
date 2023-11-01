2195cf0571b599b5628145d940d48eb1fa55a356
junseo <djawnstj44@naver.com>
2023-11-01 16:56:12 +0900

    Feat: 회원 별 경력 조회하는 쿼리 추가

--- src/main/kotlin/com/interrupt/server/career/repository/CareerQueryRepository.kt
+++ src/main/kotlin/com/interrupt/server/career/repository/CareerQueryRepository.kt
@@ -1,8 +1,25 @@
 package com.interrupt.server.career.repository
 
+import com.interrupt.server.career.entity.Career
+import com.interrupt.server.member.entity.Member
 import org.springframework.stereotype.Repository
 
 @Repository
 class CareerQueryRepository(
+    private val careerRepository: CareerRepository,
 ) {
+
+    fun findAllByMemberId(memberId: Long): List<Career> {
+        return careerRepository.findAll {
+            select(
+                entity(Career::class)
+            ).from(
+                entity(Career::class),
+                fetchJoin(Member::class).on(path(Member::id).equal(path(Career::member)(Member::id)))
+            ).where(
+                path(Career::member)(Member::id).equal(memberId)
+            )
+        }.filterNotNull()
+    }
+
 }
