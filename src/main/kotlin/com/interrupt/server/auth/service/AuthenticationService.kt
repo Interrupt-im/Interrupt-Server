3b0479226d81821c9c025240be9fba3b9d2a2e32
junseo <djawnstj44@naver.com>
2024-01-07 22:27:26 +0900

    Feat: 인증 로직 서비스 생성
    로그인, 토큰 새로고침 등 jwt 인증과 관련된 역할을 담당하는 서비스를 개발했다.

---
+++ src/main/kotlin/com/interrupt/server/auth/service/AuthenticationService.kt
@@ -0,0 +1,89 @@
+package com.interrupt.server.auth.service
+
+import com.interrupt.server.auth.config.JwtProperties
+import com.interrupt.server.auth.dto.request.TokenRefreshRequest
+import com.interrupt.server.auth.dto.response.TokenRefreshResponse
+import com.interrupt.server.auth.entity.AuthenticationCredentials
+import com.interrupt.server.auth.entity.TokenCache
+import com.interrupt.server.auth.repository.TokenRedisRepository
+import com.interrupt.server.common.exception.ErrorCode
+import com.interrupt.server.common.exception.InterruptServerException
+import com.interrupt.server.member.dto.login.SignInRequest
+import com.interrupt.server.member.dto.login.SignInResponse
+import com.interrupt.server.member.entity.Member
+import com.interrupt.server.member.repository.MemberRepository
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class AuthenticationService(
+    private val tokenRedisRepository: TokenRedisRepository,
+    private val memberQueryRepository: MemberRepository,
+    private val jwtService: JwtService,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtProperties: JwtProperties,
+) {
+
+    fun login(request: SignInRequest): SignInResponse {
+        val member: Member = authenticationManager.authenticate(
+                UsernamePasswordAuthenticationToken(request.loginId, request.password)
+        ).principal as Member
+
+        val authenticationCredentials = jwtService.generateAuthenticationCredentials(member)
+
+        saveTokenCache(authenticationCredentials.identifier.public, authenticationCredentials)
+
+        return authenticationCredentials.credentials.run { SignInResponse(accessToken, refreshToken) }
+    }
+
+    fun refreshToken(request: TokenRefreshRequest): TokenRefreshResponse {
+        val presentedRefreshToken = request.refreshToken
+
+        val loginId: String = jwtService.extractUsername(presentedRefreshToken)
+        val jti = jwtService.extractJti(presentedRefreshToken)
+
+        val user = memberQueryRepository.findByLoginId(loginId).isValidMember()
+
+        val foundTokens = tokenRedisRepository.findById(jti).isValidToken()
+
+        validateRefreshToken(presentedRefreshToken, foundTokens.credentials.refreshToken, user)
+
+        validateActiveAccessToken(foundTokens.credentials.accessToken, user)
+
+        val authenticationCredentials = jwtService.generateAuthenticationCredentials(user)
+
+        saveTokenCache(authenticationCredentials.identifier.public, authenticationCredentials)
+        tokenRedisRepository.deleteById(jti)
+
+        return authenticationCredentials.credentials.run { TokenRefreshResponse(accessToken, refreshToken) }
+    }
+
+    private fun validateActiveAccessToken(accessToken: String, member: Member) {
+        if (!jwtService.checkTokenExpiredByTokenString(accessToken)) throw InterruptServerException(ErrorCode.SUSPICIOUS_ACTIVITY_DETECTED)
+        tokenRedisRepository.deleteById(member.loginId)
+    }
+
+    private fun saveTokenCache(uuid: String, authenticationCredentials: AuthenticationCredentials) {
+        val tokenCache = TokenCache(uuid, authenticationCredentials, jwtProperties.refreshTokenExpiration)
+
+        tokenRedisRepository.save(tokenCache)
+    }
+
+    private fun validateRefreshToken(jwt: String, refreshToken: String, user: UserDetails) {
+        if (jwt != refreshToken) throw InterruptServerException(ErrorCode.SUSPICIOUS_ACTIVITY_DETECTED)
+        if (!jwtService.isTokenValid(jwt, user)) throw InterruptServerException(ErrorCode.SUSPICIOUS_ACTIVITY_DETECTED)
+    }
+
+    private fun Member?.isValidMember(): Member = this ?: throw InterruptServerException(ErrorCode.SUSPICIOUS_ACTIVITY_DETECTED)
+
+    private fun AuthenticationCredentials?.isValidToken(): AuthenticationCredentials =
+        this?.let { this } ?: throw throw InterruptServerException(ErrorCode.SUSPICIOUS_ACTIVITY_DETECTED)
+
+}
+
+
+
