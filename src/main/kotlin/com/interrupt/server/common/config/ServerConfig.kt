3296a3f5690f1acb8cb3740e96595d8cddbfd9da
junseo <djawnstj44@naver.com>
2024-01-07 22:30:10 +0900

    Feat: JWT 관련 설정 추가

--- src/main/kotlin/com/interrupt/server/common/config/ServerConfig.kt
+++ src/main/kotlin/com/interrupt/server/common/config/ServerConfig.kt
@@ -1,15 +1,45 @@
 package com.interrupt.server.common.config
 
+import com.interrupt.server.member.repository.MemberRepository
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class ServerConfig: WebMvcConfigurer {
+class ServerConfig(
+    private val memberRepository: MemberRepository
+): WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000")
     }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService = UserDetailsService {
+        memberRepository.findByLoginId(it) ?: throw UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다.")
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider =
+        DaoAuthenticationProvider().apply {
+            setUserDetailsService(userDetailsService())
+            setPasswordEncoder(passwordEncoder())
+        }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
 
 }
