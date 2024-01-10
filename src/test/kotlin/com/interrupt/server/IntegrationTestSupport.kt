998336a1f9de793b8955748279db2057e5456227
JUNSEO <djawnstj44@naver.com>
2024-01-10 16:41:55 +0900

    Test: test 용 환경 설정 추가

--- src/test/kotlin/com/interrupt/server/IntegrationTestSupport.kt
+++ src/test/kotlin/com/interrupt/server/IntegrationTestSupport.kt
@@ -1,7 +1,9 @@
 package com.interrupt.server
 
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 abstract class IntegrationTestSupport {
 }
