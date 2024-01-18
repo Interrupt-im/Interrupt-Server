662af4c10b277f12f1befc9ca1dd1b3454270c84
JUNSEO <djawnstj44@naver.com>
2024-01-18 17:19:44 +0900

    Test: JwtServiceTest 수정
    TokenProvider 등 JwtService 구조 변경에 맞춰
    테스트 코드에도 TokenProvider 구현체를 만들었다.

--- src/test/kotlin/com/interrupt/server/auth/service/JwtServiceTest.kt
+++ src/test/kotlin/com/interrupt/server/auth/service/JwtServiceTest.kt
@@ -3,12 +3,41 @@ package com.interrupt.server.auth.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.interrupt.server.auth.config.JwtProperties
 import io.mockk.mockk
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.*
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 class JwtServiceTest {
 
-    private val jwtProperties: JwtProperties = mockk()
+    class MockTokenProvider: TokenProvider {
+
+        private val keyBytes = ByteArray(256 / 8) { 0x01.toByte() }
+        override val secretKey: SecretKey = SecretKeySpec(keyBytes, "HmacSHA256")
+
+        override fun extractUsername(token: String): String? {
+            TODO("Not yet implemented")
+        }
+
+        override fun extractJti(token: String): String? {
+            TODO("Not yet implemented")
+        }
+
+        override fun extractExpiration(token: String): Date? {
+            TODO("Not yet implemented")
+        }
+
+        override fun buildToken(userDetails: UserDetails, jti: String, expiration: Long, additionalClaims: Map<String, Any>): String {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    private val tokenProvider: TokenProvider = MockTokenProvider()
+    private val jwtProperties: JwtProperties = mockk(relaxed = true)
     private val objectMapper: ObjectMapper = mockk()
 
-    private val jwtService: JwtService = JwtService(jwtProperties, objectMapper)
+    private val jwtService: JwtService = JwtService(tokenProvider, jwtProperties, objectMapper)
+
 
 }
