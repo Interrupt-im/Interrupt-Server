84b8cb74c26b47a9be39a773df008b0b5ce16379
JUNSEO <djawnstj44@naver.com>
2024-08-12 10:23:35 +0900

    chore: spring security 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,6 @@ dependencies {
     // Validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    // 시큐리티
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    testImplementation("org.springframework.security:spring-security-test")
-
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
