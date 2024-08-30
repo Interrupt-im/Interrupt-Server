689fa3971c02fa60d3af4d7059607a008db50766
JUNSEO <djawnstj44@naver.com>
2024-08-30 09:56:59 +0900

    feat: 요청 예외 처리 추가

--- src/main/kotlin/com/interrupt/server/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/interrupt/server/global/exception/ErrorCode.kt
@@ -19,4 +19,6 @@ enum class ErrorCode(
 
     // COMMON
     INTERNAL_SEVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "관리자에게 문의 바랍니다."),
+    NO_CONTENT_HTTP_BODY(HttpStatus.BAD_REQUEST, "정상적인 요청 본문이 아닙니다."),
+    NOT_SUPPORTED_METHOD(HttpStatus.METHOD_NOT_ALLOWED, "정상적인 요청이 아닙니다."),
 }
--- src/main/kotlin/com/interrupt/server/global/exception/ExceptionAdvice.kt
+++ src/main/kotlin/com/interrupt/server/global/exception/ExceptionAdvice.kt
@@ -2,7 +2,10 @@ package com.interrupt.server.global.exception
 
 import com.interrupt.server.global.common.ErrorResponse
 import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.validation.ConstraintViolationException
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -14,6 +17,14 @@ class ExceptionAdvice {
     @ExceptionHandler(ApplicationException::class)
     fun handleApplicationException(e: ApplicationException): ResponseEntity<ErrorResponse> =
         ErrorResponseEntity(e.errorCode, e)
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> =
+        ErrorResponseEntity(ErrorCode.NO_CONTENT_HTTP_BODY, e)
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleHttpRequestMethodNotSupportedException(e: HttpRequestMethodNotSupportedException): ResponseEntity<ErrorResponse> =
+        ErrorResponseEntity(ErrorCode.NOT_SUPPORTED_METHOD, e)
 
     @ExceptionHandler(Throwable::class)
     fun handleThrowable(t: Throwable): ResponseEntity<ErrorResponse> =
