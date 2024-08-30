09cc91e3743830cf60a79c39d1d12cc0640f636f
JUNSEO <djawnstj44@naver.com>
2024-08-30 15:22:40 +0900

    feat: MemberService 트랜잭션 적용
    MemberCommandService::createMember 메서드에서 불필요한 커넥션 낭비를 줄이기 위해 @Transactional 메서드로 묶어주었다.

--- src/main/kotlin/com/interrupt/server/member/application/MemberCommandService.kt
+++ src/main/kotlin/com/interrupt/server/member/application/MemberCommandService.kt
@@ -5,6 +5,7 @@ import com.interrupt.server.global.exception.ErrorCode
 import com.interrupt.server.member.application.command.MemberCreateCommand
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class MemberCommandService(
@@ -12,6 +13,8 @@ class MemberCommandService(
     private val memberQueryRepository: MemberQueryRepository,
     private val passwordEncoder: PasswordEncoder,
 ) {
+
+    @Transactional
     fun createMember(memberCreateCommand: MemberCreateCommand): Long {
         if (isAlreadyExistEmail(memberCreateCommand.email)) {
             throw ApplicationException(ErrorCode.DUPLICATED_REGISTER_EMAIL)
