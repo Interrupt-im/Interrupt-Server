276e9c0a13bd97b168f81aefb7a40382090dd3ef
JUNSEO <djawnstj44@naver.com>
2024-09-03 16:21:23 +0900

    feat: AuthCommandService 추가

---
+++ src/main/kotlin/com/interrupt/server/auth/application/AuthCommandService.kt
@@ -0,0 +1,38 @@
+package com.interrupt.server.auth.application
+
+import com.interrupt.server.auth.application.command.LoginCommand
+import com.interrupt.server.auth.domain.AuthenticationCredentials
+import com.interrupt.server.global.exception.ApplicationException
+import com.interrupt.server.global.exception.ErrorCode
+import com.interrupt.server.member.application.MemberQueryRepository
+import com.interrupt.server.member.application.PasswordService
+import com.interrupt.server.member.domain.Member
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class AuthCommandService(
+    private val memberQueryRepository: MemberQueryRepository,
+    private val passwordService: PasswordService,
+    private val jwtService: JwtService,
+    private val tokenCommandRepository: TokenCommandRepository,
+) {
+
+    fun login(loginCommand: LoginCommand): AuthenticationCredentials {
+        val member = memberQueryRepository.findByEmailAndNotDeleted(loginCommand.email).validateLoginField(loginCommand)
+
+        val authenticationCredentials = jwtService.generateAuthenticationCredentials(member)
+        tokenCommandRepository.save(authenticationCredentials)
+
+        return authenticationCredentials
+    }
+
+    private fun Member?.validateLoginField(loginCommand: LoginCommand): Member {
+        check(this != null&& passwordService.matches(loginCommand.password, password)) {
+            throw ApplicationException(ErrorCode.NOT_MATCH_LOGIN_FIELD)
+        }
+
+        return this
+    }
+}
