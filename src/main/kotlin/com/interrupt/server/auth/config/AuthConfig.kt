6837efa8fb7fd92be8596a17b598ded97448916d
JUNSEO <djawnstj44@naver.com>
2024-09-10 16:12:41 +0900

    feat: 인가 ArgumentResolver 등록

--- src/main/kotlin/com/interrupt/server/auth/config/AuthConfig.kt
+++ src/main/kotlin/com/interrupt/server/auth/config/AuthConfig.kt
@@ -1,9 +1,24 @@
 package com.interrupt.server.auth.config
 
 import com.interrupt.server.auth.application.JwtProperties
+import com.interrupt.server.auth.application.JwtService
+import com.interrupt.server.auth.application.TokenQueryRepository
+import com.interrupt.server.auth.presentation.LoginUserArgumentResolver
+import com.interrupt.server.member.application.MemberDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
-class AuthConfig
+class AuthConfig(
+    private val jwtService: JwtService,
+    private val tokenQueryRepository: TokenQueryRepository,
+    private val memberDetailsService: MemberDetailsService,
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
+        argumentResolvers.add(LoginUserArgumentResolver(jwtService, tokenQueryRepository, memberDetailsService))
+    }
+}
