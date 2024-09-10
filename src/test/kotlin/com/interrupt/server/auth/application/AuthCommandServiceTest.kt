6d28eecff2fcba75a5c8398b8b7adf85a4477f54
JUNSEO <djawnstj44@naver.com>
2024-09-10 16:14:11 +0900

    test: AuthCommandService::logout 테스트

--- src/test/kotlin/com/interrupt/server/auth/application/AuthCommandServiceTest.kt
+++ src/test/kotlin/com/interrupt/server/auth/application/AuthCommandServiceTest.kt
@@ -1,7 +1,8 @@
 package com.interrupt.server.auth.application
 
 import com.interrupt.server.auth.fake.FakeJwtService
-import com.interrupt.server.auth.fake.FakeTokenCommandRepository
+import com.interrupt.server.auth.fake.FakeTokenRepository
+import com.interrupt.server.auth.fixture.TokenFixture
 import com.interrupt.server.global.exception.ApplicationException
 import com.interrupt.server.member.fake.FakeMemberQueryRepository
 import com.interrupt.server.member.fake.FakePasswordService
@@ -10,16 +11,19 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
 
 @DisplayName("AuthCommandService 단위 테스트")
 class AuthCommandServiceTest : BehaviorSpec({
 
     val memberQueryRepository = FakeMemberQueryRepository()
-    val authCommandService = AuthCommandService(memberQueryRepository, FakePasswordService(), FakeJwtService(), FakeTokenCommandRepository())
+    val tokenCommandRepository = FakeTokenRepository()
+    val authCommandService = AuthCommandService(memberQueryRepository, FakePasswordService(), FakeJwtService(), tokenCommandRepository)
 
     beforeAny {
         memberQueryRepository.init()
+        tokenCommandRepository.init()
     }
 
     Given("로그인 요청을 받았을 때") {
@@ -44,4 +48,17 @@ class AuthCommandServiceTest : BehaviorSpec({
         }
     }
 
+    Given("로그아웃 요청을 받으면") {
+        val command = TokenFixture.`토큰 1`.`로그 아웃 COMMAND 생성`()
+
+        When("로그인 회원의 토큰을 이용해 토큰을 삭제한 후엔") {
+            authCommandService.logout(command)
+
+            val actual = tokenCommandRepository.findByJti("jti1")
+
+            Then("저장소에서 해당 토큰을 조회할 수 없다") {
+                actual shouldBe null
+            }
+        }
+    }
 })
