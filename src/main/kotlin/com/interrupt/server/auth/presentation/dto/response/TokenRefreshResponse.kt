1fb972f809fbad4367456c3d557dc131e34d2022
junseo <djawnstj44@naver.com>
2024-09-22 22:59:35 +0900

    refactor: 토큰 재발급 응답 객체 프로퍼티 추가

--- src/main/kotlin/com/interrupt/server/auth/presentation/dto/response/TokenRefreshResponse.kt
+++ src/main/kotlin/com/interrupt/server/auth/presentation/dto/response/TokenRefreshResponse.kt
@@ -3,7 +3,8 @@ package com.interrupt.server.auth.presentation.dto.response
 import com.interrupt.server.auth.domain.AuthenticationCredentials
 
 data class TokenRefreshResponse(
+    val accessToken: String,
     val refreshToken: String,
 ) {
-    constructor(authenticationCredentials: AuthenticationCredentials) : this(authenticationCredentials.refreshToken)
+    constructor(authenticationCredentials: AuthenticationCredentials) : this(authenticationCredentials.accessToken, authenticationCredentials.refreshToken)
 }
