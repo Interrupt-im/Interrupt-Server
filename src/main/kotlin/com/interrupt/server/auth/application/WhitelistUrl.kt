61ced3502a7313a03c69dddf4881952cf2138e57
JUNSEO <djawnstj44@naver.com>
2024-09-23 16:49:15 +0900

    refactor: WhitelistUrl url 매칭, 메서드 매칭 로직 수정

--- src/main/kotlin/com/interrupt/server/auth/application/WhitelistUrl.kt
+++ src/main/kotlin/com/interrupt/server/auth/application/WhitelistUrl.kt
@@ -1,11 +1,12 @@
 package com.interrupt.server.auth.application
 
+import com.interrupt.server.global.util.matchUrl
 import com.interrupt.server.member.domain.MemberType
 import org.springframework.http.HttpMethod
 
 data class WhitelistUrl(
     val url: String,
-    val methods: List<HttpMethod>,
+    val methods: List<HttpMethod> = listOf(),
     val role: MemberType = MemberType.CUSTOMER,
 ) {
     fun match(uri: String, method: String): Boolean = isMatchUri(uri) && isMatchMethod(method)
@@ -13,8 +14,8 @@ data class WhitelistUrl(
     private fun isMatchMethod(method: String): Boolean {
         val httpMethod = HttpMethod.valueOf(method)
 
-        return methods.any { it == httpMethod }
+        return methods.isEmpty() || methods.any { it == httpMethod }
     }
 
-    private fun isMatchUri(uri: String): Boolean = uri.startsWith(this.url) || uri.matches(this.url.toRegex())
+    private fun isMatchUri(uri: String): Boolean = matchUrl(this.url, uri)
 }
