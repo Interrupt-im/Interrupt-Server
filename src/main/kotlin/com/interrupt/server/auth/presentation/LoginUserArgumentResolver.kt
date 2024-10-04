f5b4d66894e6cdb23a5ae86b591bfdfde4a4f9b7
JUNSEO <djawnstj44@naver.com>
2024-10-04 16:17:46 +0900

    refactor: LoginUserArgumentResolver 책임 분리
    기존 LoginUserArgumentResolver 클래스에 요청 헤더의 토큰을 검증하던 로직을 UrlWhitelistInterceptor 에게 위임하였다.

--- src/main/kotlin/com/interrupt/server/auth/presentation/LoginUserArgumentResolver.kt
+++ src/main/kotlin/com/interrupt/server/auth/presentation/LoginUserArgumentResolver.kt
@@ -1,7 +1,5 @@
 package com.interrupt.server.auth.presentation
 
-import com.interrupt.server.auth.application.JwtService
-import com.interrupt.server.auth.application.TokenQueryRepository
 import com.interrupt.server.auth.application.UserDetailsService
 import com.interrupt.server.auth.domain.Token
 import com.interrupt.server.global.exception.ApplicationException
@@ -9,44 +7,38 @@ import com.interrupt.server.global.exception.ErrorCode
 import org.springframework.core.MethodParameter
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.context.request.RequestAttributes
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 
 class LoginUserArgumentResolver(
-    private val jwtService: JwtService,
-    private val tokenQueryRepository: TokenQueryRepository,
     private val userDetailsService: UserDetailsService,
 ) : HandlerMethodArgumentResolver {
     override fun supportsParameter(parameter: MethodParameter): Boolean = parameter.hasParameterAnnotation(LoginUser::class.java)
 
     override fun resolveArgument(parameter: MethodParameter, mavContainer: ModelAndViewContainer?, webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?): Any {
-        val authorization = webRequest.getHeader(AUTH_HEADER_NAME)
+        val (username, token) = getAuthAttribute(webRequest)
 
-        if (!authorization.isNullOrBlank() && authorization.startsWith(AUTH_HEADER_PREFIX)) {
-            val token = authorization.substring(AUTH_HEADER_PREFIX.length)
-            val username = jwtService.getUsername(token)
-            val jti = jwtService.getJti(token)
-            val savedToken = tokenQueryRepository.findByJti(jti)
+        val userDetails = userDetailsService.loadUserByUsername(username) ?: throw ApplicationException(ErrorCode.INVALID_ACCESS_TOKEN)
+        userDetails.setToken(token)
 
-            checkValidAccessToken(token, savedToken)
-
-            val userDetails = userDetailsService.loadUserByUsername(username) ?: throw ApplicationException(ErrorCode.INVALID_ACCESS_TOKEN)
-            userDetails.setToken(savedToken!!)
-
-            return userDetails
-        }
-
-        throw ApplicationException(ErrorCode.INVALID_ACCESS_TOKEN)
+        return userDetails
     }
 
-    private fun checkValidAccessToken(token: String, savedToken: Token?) {
-        check(savedToken != null && savedToken.accessToken == token) {
-            throw ApplicationException(ErrorCode.INVALID_ACCESS_TOKEN)
+    private fun getAuthAttribute(webRequest: NativeWebRequest): Pair<String, Token> {
+        return webRequest.let { request ->
+            val username = request.getAttribute("username", RequestAttributes.SCOPE_REQUEST)
+            val token = request.getAttribute("token", RequestAttributes.SCOPE_REQUEST)
+
+            checkAuthAttribute(username, token)
+
+            return@let username.toString() to token as Token
         }
     }
 
-    companion object {
-        private const val AUTH_HEADER_NAME = "Authorization"
-        private const val AUTH_HEADER_PREFIX = "Bearer "
+    private fun checkAuthAttribute(username: Any?, token: Any?) {
+        require(username != null && token != null) {
+            throw ApplicationException(ErrorCode.UNAUTHORIZED_ACCESS_ATTEMPT)
+        }
     }
 }
